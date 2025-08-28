f0e7b98a41636537c9b3b6b3135c5451c9200dcb
Meet Makadia <mrmakadia94@gmail.com>
2025-08-28 11:25:21 +0530

    remove build folder

--- build/blocks/before-after-slider/view.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => '512e5f1dbbae2026a309');
--- build/blocks/image-stack/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n'), 'version' => '9c2bf67c8f7a34e22fc5');
