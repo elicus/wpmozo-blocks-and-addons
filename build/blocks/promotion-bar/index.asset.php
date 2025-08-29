9b5e11bf3d85d1665adb49f60d19e90219dfd115
Jaydip N <jaydeep.nimavat@gmail.com>
2025-08-29 09:19:46 +0530

    fixed conflict on promotion bar

--- build/blocks/promotion-bar/index.asset.php
+++ build/blocks/promotion-bar/index.asset.php
@@ -1,1 +1,5 @@
+<<<<<<< HEAD
 <?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-date', 'wp-element', 'wp-i18n'), 'version' => '9681aca1ea966de56a18');
+=======
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-date', 'wp-element', 'wp-i18n'), 'version' => '119f99c154e2d04ffbd4');
+>>>>>>> f3e4499 (Promotion bar completed/)
