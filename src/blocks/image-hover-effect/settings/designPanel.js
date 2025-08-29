879824b84ce14b9f1362a281079e6ee93b5712f0
himanshudhakan <himanshudhakan9@gmail.com>
2025-08-29 20:39:48 +0530

    WIP: image-hover-effect settings

---
+++ src/blocks/image-hover-effect/settings/designPanel.js
@@ -0,0 +1,20 @@
+import { __ } from "@wordpress/i18n";
+import {
+	PanelBody
+} from "@wordpress/components";
+import {
+	WpmozoBorder
+} from '../../../common/components/index';
+
+export const DesignPanel = ( { attributes, setAttributes } ) => {
+	const props = { attributes, setAttributes, preAttributes: {} };
+
+	return ( <>
+		<PanelBody title={ __( 'Image Setting', 'wpmozo-blocks-and-addons' ) } className="wpmozo-typography-panel" initialOpen={false}>
+			<WpmozoBorder
+				props={props}
+				BorderKey="image"
+			/>
+		</PanelBody>
+	</> );
+};
