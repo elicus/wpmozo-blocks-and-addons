c8fb9d0c187046d995d2f9e1eb5a1154f8352265
himanshudhakan <himanshudhakan9@gmail.com>
2025-09-01 13:09:14 +0530

    added full width setting

--- src/blocks/image-hover-effect/style.js
+++ src/blocks/image-hover-effect/style.js
@@ -10,8 +10,13 @@ const generateDynamicStyle = ({attributes, clientId}) => {
 	let styles = `#block-${attributes.ID}{`;
 
 	styles += `.wpmozo-bna-image-hover-effect-inner{ ${convertedStyle.image} }`;
-	
+
 	styles += `}`;
+
+	if ( attributes.forceFullwidth ) {
+		styles += `#block-${attributes.ID}{width: 100%; max-width: 100% !important;}.wpmozo-bna-image-hover-effect-inner, .wpmozo-bna-image-hover-effect-inner img{width: 100%;}`;
+	}
+
 	return styles;
 };
 
