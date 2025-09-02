2ecbdef4bf77cceec61f1e2ed1e2a037fd66fac3
himanshudhakan <himanshudhakan9@gmail.com>
2025-09-02 16:04:24 +0530

    WIP: edit rotating text

---
+++ src/blocks/rotating-text/edit.js
@@ -0,0 +1,52 @@
+import { __ } from "@wordpress/i18n";
+import { useEffect, Fragment } from "@wordpress/element";
+import { useBlockProps } from "@wordpress/block-editor";
+import Inspector from "./inspector";
+
+import generateDynamicStyle from "./style";
+
+export default function Edit( props ) {
+
+	const { attributes, setAttributes, clientId } = props;
+
+	let image = ( attributes.image ) ? attributes.image : wpmozo_bna_editor_object.placeholderImg,	
+		useImage = attributes.useImage,
+		text = attributes.rotatingText,
+		iconImage = null;
+
+	if ( useImage === "on" ) {
+		iconImage = (
+		  	<img
+				src={image}
+				alt=""
+				className="wpmozo-bna-rotating-text-img-img_icon"
+		  	/>
+		);
+	} else if ( props.icon ) {
+		iconImage = (
+			<div className="icon-wrapper">
+				<i className={attributes.icon}></i>
+		    </div>
+		);
+	}
+
+	return (
+		<Fragment>
+			<Inspector attributes={attributes} setAttributes={setAttributes} />
+			<style>{ generateDynamicStyle( { attributes, clientId } ) }</style>
+
+			<div id={`block-${attributes.ID}`} { ...useBlockProps( { className: 'wpmozo-bna-rotating-text' } ) }>
+				<div className="wpmozo-bna-rotating-text-wrap">
+		            <div className="wpmozo-bna-rotating-text-inner">
+		                <p>{text}</p>
+		            </div>
+		            {iconImage && (
+		                <div className="wpmozo-bna-rotating-text-icon-wrapper">
+		                    {iconImage}
+		                </div>
+		            )}
+		        </div>
+			</div>
+		</Fragment>
+	);
+}
