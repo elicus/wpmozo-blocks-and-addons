cdd3ddf4a150a8bae5b3c363bbf598262c44d360
Meet Makadia <mrmakadia94@gmail.com>
2025-09-08 11:19:12 +0530

    block added advanced-button

---
+++ src/blocks/advanced-button/edit.js
@@ -0,0 +1,57 @@
+import {__} from '@wordpress/i18n';
+import {useBlockProps} from '@wordpress/block-editor';
+import Inspector from './inspector';
+/**
+ * Lets webpack process CSS, SASS or SCSS files referenced in JavaScript files.
+ * Those files can contain any CSS code that gets applied to the editor.
+ *
+ * @see https://www.npmjs.com/package/@wordpress/scripts#using-css
+ */
+import './editor.scss';
+import generateDynamicStyle from "./style";
+
+export default function Edit(props) {
+
+	let attributes = props.attributes,
+		clientId = props.clientId,
+		setAttributes = props.setAttributes,
+		showOnHover = (attributes.showMediaOnHover) ? ' show-on-hover' : '',
+		bkStyle = attributes.backgroundFillStyle ? ` wpmozo_button_${attributes.backgroundFillStyle}` : '';
+
+	attributes.ID = clientId;
+
+	return (
+		<>
+			<Inspector attributes={attributes} setAttributes={setAttributes}/>
+			<style>
+				{generateDynamicStyle({attributes})}
+			</style>
+			<div {...useBlockProps()} id={`block-${attributes.ID}`}>
+				<div className="wpmozo-button-container">
+					<div className="wpmozo-button-item">
+						<div className="wpmozo-button-wrapper">
+							<a
+								className={`wpmozo-button-link${showOnHover}${bkStyle}`}
+								href={attributes.buttonUrl}
+								{...(attributes.buttonLinkTarget !== 'same' ? {target: "_blank"} : {})}
+							>
+								<span className="wpmozo-primary-text-with-icon">
+									<span
+										className="wpmozo-button-text wpmozo-button-icon">{attributes.buttonText}</span>
+									{true === attributes.iconButton && (
+										<i className={attributes.icon + ' wpmozo-bna-button-icon'}></i>
+									)}
+								</span>
+								{attributes.buttonSecondaryText && (
+									<span
+										className="wpmozo-button-secondary-text">{attributes.buttonSecondaryText}</span>
+								)}
+								<span className="wpmozo-background-effect-wrap"></span>
+							</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</>
+	);
+}
