ee4ebea27f7039c3ea895242e616261b72cb64cb
himanshudhakan <himanshudhakan9@gmail.com>
2025-09-10 20:24:04 +0530

    fix: image-hover-effect side glitch issue

--- src/blocks/image-hover-effect/style.js
+++ src/blocks/image-hover-effect/style.js
@@ -6,11 +6,27 @@ const generateDynamicStyle = ({attributes, clientId}) => {
 		'image'
 	];
     let convertedStyle = convertInlineStyleStr( toConvertStyles, attributes );
+    let image = ( attributes.image ) ? attributes.image : wpmozo_bna_editor_object.placeholderImg,
+    	hoverEffect = attributes.hoverEffect;
 
 	let styles = `#block-${attributes.ID}{`;
 
 	styles += `.wpmozo-bna-image-hover-effect-inner{ ${convertedStyle.image} }`;
+		if ( 'glitch' === hoverEffect ) {
+			styles += `
+				.wpmozo-bna-glitch-image::before {
+					background-image: url('${image}');
+				}
+			`;
+		}
 
+		if ( 'slide_glitch' === hoverEffect ) {
+			styles += `
+				.wpmozo-bna-slide-glitch{
+					background-image: url('${image}');
+				}
+			`;
+		}
 	styles += `}`;
 
 	if ( attributes.forceFullwidth ) {
