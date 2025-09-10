01ccb189778b50943ce02ecb53ba444660921244
himanshudhakan <himanshudhakan9@gmail.com>
2025-09-10 17:48:30 +0530

    fix: image-hover-effect structure issue

--- build/blocks/price-list-item/index.asset.php
+++ build/blocks/price-list-item/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'b0ee8f02a8d89373d174');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'e9bb1580bf5e8771a6e9');
