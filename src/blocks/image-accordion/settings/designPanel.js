b495f2e2bacb18051f62acb02c5609187f121db5
himanshudhakan <himanshudhakan9@gmail.com>
2025-09-19 17:56:43 +0530

    removed position from button

--- src/blocks/image-accordion/settings/designPanel.js
+++ src/blocks/image-accordion/settings/designPanel.js
@@ -164,6 +164,7 @@ export const DesignPanel = ( { attributes, setAttributes } ) => {
 			/>
 			<WpmozoDimensions props={props}
 				DimensionKey='button'
+				DimensionsTypes={ { padding: true, margin: true } }
 			/>
 		</PanelBody>
 	</> );
