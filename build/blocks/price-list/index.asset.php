53848f6794623657dfc103e70e55d3c0851ac53c
Jaydip N <jaydeep.nimavat@gmail.com>
2025-10-08 18:47:41 +0530

    Added global P support.

--- build/blocks/price-list/index.asset.php
+++ build/blocks/price-list/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '0eb2137f7477ef82bb79');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '7902122df5382d4c7e3f');
