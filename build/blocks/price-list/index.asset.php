873e7c5efef2c483cfba82829996a6988b9ee57c
Jaydip N <jaydeep.nimavat@gmail.com>
2025-10-13 18:25:00 +0530

    Delete build/blocks directory

--- build/blocks/price-list/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'a3f44d9b44c502bd1332');
