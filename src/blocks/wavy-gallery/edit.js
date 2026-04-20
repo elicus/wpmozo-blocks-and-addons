d501b6b0955642825cd827739f6407b2b0e2d0ad
Meet Makadia <mrmakadia94@gmail.com>
2026-04-20 19:47:38 +0530

    Update edit.js

--- src/blocks/wavy-gallery/edit.js
+++ src/blocks/wavy-gallery/edit.js
@@ -2,9 +2,8 @@
  * External dependencies
  */
 import { concat, find } from 'lodash';
-import {Fragment} from "@wordpress/element";
+import { Fragment } from "@wordpress/element";
 import { View } from '@wordpress/primitives';
-import { toHTMLString } from '@wordpress/rich-text';
 
 /**
  * WordPress dependencies
@@ -14,12 +13,13 @@ import { compose } from '@wordpress/compose';
 import { createBlobURL } from '@wordpress/blob';
 import { createBlock } from '@wordpress/blocks';
 import { store as blockEditorStore, MediaPlaceholder, useBlockProps, useInnerBlocksProps, InnerBlocks } from '@wordpress/block-editor';
-import {Notice} from '@wordpress/components';
+import { Notice } from '@wordpress/components';
 import { Platform, useEffect, useMemo } from '@wordpress/element';
 import { useDispatch, useSelect } from '@wordpress/data';
 
 import Inspector from './inspector';
 import generateDynamicStyle from './style';
+
 /**
  * Lets webpack process CSS, SASS or SCSS files referenced in JavaScript files.
  * Those files can contain any CSS code that gets applied to the editor.
@@ -27,14 +27,14 @@ import generateDynamicStyle from './style';
  * @see https://www.npmjs.com/package/@wordpress/scripts#using-css
  */
 import './editor.scss';
-import {pickRelevantMediaFiles} from "../../common/components/wpmozo-block-gallery/shared-helpers";
+import { pickRelevantMediaFiles } from "../../common/components/wpmozo-block-gallery/shared-helpers";
 
 const ALLOWED_MEDIA_TYPES = [ 'image' ];
 const PLACEHOLDER_TEXT = Platform.isNative
 	? __( 'ADD MEDIA', 'wpmozo-blocks-and-addons' )
 	: __( 'Drag images, upload new ones or select files from your library.', 'wpmozo-blocks-and-addons' );
 
-export default function Edit(props) {
+export default function Edit( props ) {
 
 	const {
 		setAttributes,
@@ -47,223 +47,219 @@ export default function Edit(props) {
 	const {
 		replaceInnerBlocks,
 		updateBlockAttributes,
-	} = useDispatch(blockEditorStore);
+	} = useDispatch( blockEditorStore );
 
 	// Ensure ID is set once (no render-time mutation).
-	useEffect(() => {
-		if (attributes.ID !== clientId) {
-			setAttributes({ID: clientId});
+	useEffect( () => {
+		if ( attributes.ID !== clientId ) {
+			setAttributes( { ID: clientId } );
 		}
-	}, [clientId]); // eslint-disable-line react-hooks/exhaustive-deps.
+	}, [ clientId ] ); // eslint-disable-line react-hooks/exhaustive-deps.
 
-	const {getBlock, preferredStyle} = useSelect((select) => {
-		const settings = select(blockEditorStore).getSettings();
+	const { getBlock, preferredStyle } = useSelect( ( select ) => {
+		const settings = select( blockEditorStore ).getSettings();
 		const preferredStyleVariations =
 			settings.__experimentalPreferredStyleVariations;
 		return {
-			getBlock: select(blockEditorStore).getBlock,
-			preferredStyle: preferredStyleVariations?.value?.['core/image'],
+			getBlock:       select( blockEditorStore ).getBlock,
+			preferredStyle: preferredStyleVariations?.value?.[ 'core/image' ],
 		};
-	}, []);
+	}, [] );
 
 	const innerBlockImages = useSelect(
-		(select) => {
-			return select(blockEditorStore).getBlock(clientId)?.innerBlocks;
+		( select ) => {
+			return select( blockEditorStore ).getBlock( clientId )?.innerBlocks;
 		},
-		[clientId]
+		[ clientId ]
 	);
 
 	const images = useMemo(
 		() =>
-			innerBlockImages?.map((block) => ({
-				attributes: block.attributes,
-				clientId: block.clientId,
-				fromSavedContent: Boolean(block.originalContent),
-				id: block.attributes.id,
-				url: block.attributes.url,
-				caption: toHTMLString({value: block.attributes.caption})
-			})),
-		[innerBlockImages]
+			innerBlockImages?.map( ( block ) => ( {
+				attributes:       block.attributes,
+				clientId:         block.clientId,
+				fromSavedContent: Boolean( block.originalContent ),
+				id:               block.attributes.id,
+				url:              block.attributes.url,
+				caption:          block.attributes.caption ?? '',
+			} ) ),
+		[ innerBlockImages ]
 	);
 
 	// Set attributes.images whenever images change
-	useEffect(() => {
-		if (Array.isArray(images) && images.length > 0) {
-			// setAttributes({ images });
-			setAttributes({images_data: images});
+	useEffect( () => {
+		if ( Array.isArray( images ) && images.length > 0 ) {
+			setAttributes( { images_data: images } );
 		}
-	}, [images]);
+	}, [ images ] );
 
-	useEffect(() => {
-		const event = new CustomEvent('WPMozoWavyGalleryPropsChanged');
-		window.dispatchEvent(event);
-		const iframe = document.querySelector('iframe[name="editor-canvas"]');
-		if (iframe?.contentWindow) {
-			iframe.contentWindow.dispatchEvent(event);
+	useEffect( () => {
+		const event = new CustomEvent( 'WPMozoWavyGalleryPropsChanged' );
+		window.dispatchEvent( event );
+		const iframe = document.querySelector( 'iframe[name="editor-canvas"]' );
+		if ( iframe?.contentWindow ) {
+			iframe.contentWindow.dispatchEvent( event );
 		}
-	}, [props]);
+	}, [ props ] );
 
-	useEffect(() => {
+	useEffect( () => {
 		const changedAttributes = {};
 		const blocks = [];
-		getBlock(clientId)?.innerBlocks.forEach((block) => {
-			blocks.push(block.clientId);
-			changedAttributes[block.clientId] = {className: 'masonry-brick'};
-		});
-		updateBlockAttributes(blocks, changedAttributes, true);
-	}, []);
+		getBlock( clientId )?.innerBlocks.forEach( ( block ) => {
+			blocks.push( block.clientId );
+			changedAttributes[ block.clientId ] = { className: 'masonry-brick' };
+		} );
+		updateBlockAttributes( blocks, changedAttributes, true );
+	}, [] );
 
-	function isValidFileType(file) {
+	function isValidFileType( file ) {
 		return (
 			ALLOWED_MEDIA_TYPES.some(
-				(mediaType) => file.type?.indexOf(mediaType) === 0
-			) || file.url?.indexOf('blob:') === 0
+				( mediaType ) => file.type?.indexOf( mediaType ) === 0
+			) || file.url?.indexOf( 'blob:' ) === 0
 		);
 	}
 
-	function updateImages(selectedImages) {
+	function updateImages( selectedImages ) {
 		const newFileUploads =
-			Object.prototype.toString.call(selectedImages) ===
+			Object.prototype.toString.call( selectedImages ) ===
 			'[object FileList]';
 
 		const imageArray = newFileUploads
-			? Array.from(selectedImages).map((file) => {
-				if (!file.url) {
-					return pickRelevantMediaFiles({
-						url: createBlobURL(file),
-					});
+			? Array.from( selectedImages ).map( ( file ) => {
+				if ( ! file.url ) {
+					return pickRelevantMediaFiles( {
+						url: createBlobURL( file ),
+					} );
 				}
-
 				return file;
-			})
+			} )
 			: selectedImages;
 
-		if (!!newFileUploads && !imageArray.every(isValidFileType)) {
+		if ( !! newFileUploads && ! imageArray.every( isValidFileType ) ) {
 			noticeOperations.removeAllNotices();
 			noticeOperations.createErrorNotice(
 				__(
 					'If uploading to a gallery all files need to be image formats', 'wpmozo-blocks-and-addons'
 				),
-				{id: 'gallery-upload-invalid-file'}
+				{ id: 'gallery-upload-invalid-file' }
 			);
 		}
 
 		const processedImages = imageArray
-			.filter((file) => file.url || isValidFileType(file))
-			.map((file) => {
-				if (!file.url) {
-					return pickRelevantMediaFiles({
-						url: createBlobURL(file),
-					});
+			.filter( ( file ) => file.url || isValidFileType( file ) )
+			.map( ( file ) => {
+				if ( ! file.url ) {
+					return pickRelevantMediaFiles( {
+						url: createBlobURL( file ),
+					} );
 				}
-
 				return file;
-			});
+			} );
 
 		const newOrderMap = processedImages?.reduce(
-			(result, image, index) => (
-				(result[image.id] = index), result
+			( result, image, index ) => (
+				( result[ image.id ] = index ), result
 			),
 			{}
 		);
 
-		const existingImageBlocks = !newFileUploads
-			? innerBlockImages.filter((block) =>
+		const existingImageBlocks = ! newFileUploads
+			? innerBlockImages.filter( ( block ) =>
 				processedImages.find(
-					(img) => img.id === block.attributes.id
+					( img ) => img.id === block.attributes.id
 				)
 			)
 			: innerBlockImages;
 
-		const newCaptions = Array.from(selectedImages).reduce((previous, image) => {
-			const previousReturnedObject = !!previous?.mime ? {} : previous;
-			return {...previousReturnedObject, [`${image.id}`]: image.caption};
-		});
+		const newCaptions = Array.from( selectedImages ).reduce( ( previous, image ) => {
+			const previousReturnedObject = !! previous?.mime ? {} : previous;
+			return { ...previousReturnedObject, [ `${ image.id }` ]: image.caption };
+		} );
 
-		existingImageBlocks?.forEach((image) => {
-			const compareCaption = newCaptions[image.attributes.id];
-			if (image.attributes.caption !== compareCaption) {
+		existingImageBlocks?.forEach( ( image ) => {
+			const compareCaption = newCaptions[ image.attributes.id ];
+			if ( image.attributes.caption !== compareCaption ) {
 				image.attributes.caption = compareCaption;
 			}
-		});
+		} );
 
 		const newImageList = processedImages.filter(
-			(img) =>
-				!existingImageBlocks.find(
-					(existingImg) => img.id === existingImg.attributes.id
+			( img ) =>
+				! existingImageBlocks.find(
+					( existingImg ) => img.id === existingImg.attributes.id
 				)
 		);
 
-		const newBlocks = newImageList.map((image) => {
-			return createBlock('core/image', {
-				alt: image.alt,
+		const newBlocks = newImageList.map( ( image ) => {
+			return createBlock( 'core/image', {
+				alt:     image.alt,
 				caption: image.caption,
-				id: image.id,
-				url: image.url,
-			});
-		});
+				id:      image.id,
+				url:     image.url,
+			} );
+		} );
 
 		replaceInnerBlocks(
 			clientId,
-			concat(existingImageBlocks, newBlocks).sort(
-				(a, b) =>
-					newOrderMap[a.attributes.id] -
-					newOrderMap[b.attributes.id]
+			concat( existingImageBlocks, newBlocks ).sort(
+				( a, b ) =>
+					newOrderMap[ a.attributes.id ] -
+					newOrderMap[ b.attributes.id ]
 			)
 		);
 	}
 
-	function onUploadError(message) {
+	function onUploadError( message ) {
 		noticeOperations.removeAllNotices();
-		noticeOperations.createErrorNotice(message);
+		noticeOperations.createErrorNotice( message );
 	}
 
-	const hasImages = !!images.length;
-	const hasImageIds = hasImages && images.some((image) => !!image.id);
+	const hasImages      = !! images.length;
+	const hasImageIds    = hasImages && images.some( ( image ) => !! image.id );
 	const imagesUploading = images.some(
-		(img) => !img.id && img.url?.indexOf('blob:') === 0
+		( img ) => ! img.id && img.url?.indexOf( 'blob:' ) === 0
 	);
 
 	const mediaPlaceholder = (
 		<MediaPlaceholder
 			accept="image/*"
-			addToGallery={hasImageIds}
-			allowedTypes={ALLOWED_MEDIA_TYPES}
+			addToGallery={ hasImageIds }
+			allowedTypes={ ALLOWED_MEDIA_TYPES }
 			disableMediaButtons={
-				(hasImages && !isSelected) || imagesUploading
+				( hasImages && ! isSelected ) || imagesUploading
 			}
-			handleUpload={true}
-			isAppender={hasImages}
-			labels={{
-				instructions: !hasImages && PLACEHOLDER_TEXT,
-				title: !hasImages && __('Wavy Gallery', 'wpmozo-blocks-and-addons'),
-			}}
+			handleUpload={ true }
+			isAppender={ hasImages }
+			labels={ {
+				instructions: ! hasImages && PLACEHOLDER_TEXT,
+				title:        ! hasImages && __( 'Wavy Gallery', 'wpmozo-blocks-and-addons' ),
+			} }
 			multiple
-			onError={onUploadError}
-			onSelect={updateImages}
-			value={hasImageIds ? images : {}}
+			onError={ onUploadError }
+			onSelect={ updateImages }
+			value={ hasImageIds ? images : {} }
 		/>
 	);
 
-
 	return (
 		<Fragment>
-			<Inspector attributes={attributes} setAttributes={setAttributes}/>
-			<style>{generateDynamicStyle({attributes})}</style>
+			<Inspector attributes={ attributes } setAttributes={ setAttributes } />
+			<style>{ generateDynamicStyle( { attributes } ) }</style>
 
-			<div {...useBlockProps()} id={`block-${attributes.ID}`}>
+			<div { ...useBlockProps() } id={ `block-${ attributes.ID }` }>
 				<div className="wpmozo_wavy_gallery_wrapper">
-					{attributes.images_data && (
-						<Notice status="warning" isDismissible={false}>
+					{ attributes.images_data && (
+						<Notice status="warning" isDismissible={ false }>
 							{ __( "Wavy Gallery does not work in the Visual Builder. Please check the frontend to see the effect correctly.", "wpmozo-blocks-and-addons" ) }
 						</Notice>
-					)}
+					) }
 				</div>
-				{isSelected && (
+				{ isSelected && (
 					<View className="gallery-media-placeholder-wrapper">
-						{mediaPlaceholder}
+						{ mediaPlaceholder }
 					</View>
-				)}
+				) }
 			</div>
 		</Fragment>
 	);
