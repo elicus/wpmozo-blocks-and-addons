15a48619ff08913973b9923c18d10dc616e207c9
Meet Makadia <mrmakadia94@gmail.com>
2026-04-27 11:44:34 +0530

    added image alt tab and deprecated js - image magnifier

---
+++ src/blocks/image-magnifier/deprecated/version-1.js
@@ -0,0 +1,41 @@
+import { useBlockProps } from '@wordpress/block-editor';
+import generateDynamicStyle from '../style';
+
+const deprecatedSave = ({ attributes }) => {
+	const ID = attributes.ID;
+	let image = ( attributes.image ) ? attributes.image : wpmozo_bna_editor_object.placeholderImg;
+
+	return ( <>
+		<style>{ generateDynamicStyle( { attributes } ) }</style>
+
+		<div { ...useBlockProps.save( { className: 'wpmozo-bna-image-magnifier ' + ( attributes.className || '' ) } ) }
+			id={`block-${ID}`}
+			data-speed={ attributes.lenseSpeed }
+			data-src={ image }
+		>
+			<div className={`wpmozo-bna-image-magnifier-wrapper`}>
+				<div className="magnify">
+					<img className="wpmozo-bna-image-magnifier-image zoom" src={ image } />
+				</div>
+			</div>
+		</div>
+	</> );
+};
+
+export default {
+	attributes: {
+		ID: { type: "string" },
+		className: { type: "string" },
+
+		image: { type: "string" },
+		imageAlignment: { type: "string", default: "center" },
+
+		lensWidth: { type: "number", default: 200 },
+		lenseSpeed: { type: "number", default: 100 },
+		lenseBorderWidth: { type: "number", default: 7 },
+		imageWidth: { type: "number", default: 50 },
+		imageMaxWidth: { type: "number", default: 100 },
+		lenseBorderColor: { type: "string" }
+	},
+	save: deprecatedSave,
+};
