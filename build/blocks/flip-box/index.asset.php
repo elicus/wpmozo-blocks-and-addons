2c1af027ef62f516091ac7e454166a386089b028
Sunil Kumar <sunilkumar@Sunils-Laptop.local>
2026-08-04 12:14:57 +0530

    Hover Functionality issue fixed

--- build/blocks/flip-box/index.asset.php
+++ build/blocks/flip-box/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '6bc9107e476652938ebc');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'f0104b006cb90d883cf2');
